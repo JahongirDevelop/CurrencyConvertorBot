cff3e67636d46eaa137591070b07c48f583018e5
Jahongir Sobirjonov <131335862+JahongirDevelop@users.noreply.github.com>
2024-05-29 13:35:04 +0500

    Update UserRepository.java

--- src/main/java/uz/pdp/currencyconvertor/repository/UserRepository.java
+++ src/main/java/uz/pdp/currencyconvertor/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class UserRepository {
-    static String userPath = "D:\\Users\\HP\\java G23\\TelegramBot\\src\\main\\resources\\users.json";
+    static String userPath = "YOUR_LOCAL_STORAGE/src/main/resources/users.json"; // Relative path
     static ObjectMapper objectMapper = new ObjectMapper()
             .enable(SerializationFeature.INDENT_OUTPUT)
             .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
